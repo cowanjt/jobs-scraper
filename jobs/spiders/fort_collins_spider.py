3c7b0de12e6885f26875c946cb0bca6cb0bf8227
Jade Cowan <jade.cowan@outlook.com>
2019-02-15 17:25:43 -0700

    Add fort_collins_spider

---
+++ jobs/spiders/fort_collins_spider.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+
+class FortCollinsSpiderSpider(scrapy.Spider):
+    name = 'fort_collins_spider'
+    allowed_domains = ['fcgov.csod.com']
+    start_urls = ['https://fcgov.csod.com/ats/careersite/search.aspx?site=1&c=fcgov']
+
+    def parse(self, response):
+        base_url = 'https://fcgov.csod.com/ats/careersite/'
+        pattern = r'JobDetails.aspx\?site\=[0-9]\&id\=[0-9][0-9][0-9][0-9]'
+
+        for list_item in response.css('div#ctl00_siteContent_widgetLayout_rptWidgets_ctl00_widgetContainer_ctl00_ctl00 ul li'):
+            partial_url = re.search(pattern, list_item.css('a::attr(href)').get())
+
+            yield {
+                'job_title': list_item.css('a::text').get(default='').strip(),
+                'pay_range': 'See Job Description: ' + base_url + partial_url.group(),
+                'department': 'See Job Description: ' + base_url + partial_url.group(),
+            }
+
+        # base_url = 'https://fcgov.csod.com/ats/careersite/'
+        # pattern = r'JobDetails.aspx\?site\=[0-9]\&id\=[0-9][0-9][0-9][0-9]'
+
+        # for list_item in response.css('div#ctl00_siteContent_widgetLayout_rptWidgets_ctl00_widgetContainer_ctl00_ctl00 ul li'):
+        #     partial_url = re.search(pattern, list_item.css('a::attr(href)').get())
+        #     yield response.follow(base_url + partial_url.group(), self.parse_job)
+
+    # Todo: Figure out how to traverse inconsitent DOMs. 
+    # def parse_job(self, response):
+    #     def extract_with_css(query):
+    #         return response.css(query).get(default='').strip()
+
+    #     yield {
+    #         'job_title': extract_with_css('div.cs-atscs-jobdet-rtpane strong:nth-child(1)::text'),
+    #         'pay_range': extract_with_css('//*[@id="ctl00_careerSiteContainer"]/div[3]/div[3]/p[1]/span/font[1]/strong[2]'),
+    #         'department': extract_with_css('//*[@id="ctl00_careerSiteContainer"]/div[3]/div[3]/p[1]/span/font[1]/strong[contains(.,\'Department\')]'),
+    #     }
